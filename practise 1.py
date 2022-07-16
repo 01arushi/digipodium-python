18bcb2777b39d7ec29734ce34e3e71a969e82082
01arushi <108970873+01arushi@users.noreply.github.com>
2022-07-16 17:14:53 +0530

    Create practise 1.py

---
+++ practise 1.py
@@ -0,0 +1,10 @@
+# author : arushi 
+# company : abc company 
+print("Hello world")
+# multiline comment 
+'''
+# author : arushi 
+# company : abc company
+********** thankx for reading ***********
+'''
+print ('welcome')
