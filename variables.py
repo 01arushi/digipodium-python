044269289efd8003aae87112532f6894147c5336
01arushi <108970873+01arushi@users.noreply.github.com>
2022-07-17 12:03:30 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,16 @@
+#integer
+a=56
+b=658932236652366633 
+print(a, b)
+#float point 
+pi = 3.14
+print(pi)
+# string 
+j ='D'
+name = 'arushi'
+print(j , name )
+# boolean 
+z = True
+print(z)
+o=None 
+print(o)
