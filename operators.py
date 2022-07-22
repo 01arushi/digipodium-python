49e31a16066abdf1daa825b9c1f02f00270c3c94
01arushi <108970873+01arushi@users.noreply.github.com>
2022-07-22 21:11:37 +0530

    all

---
+++ operators.py
@@ -0,0 +1,27 @@
+a=34
+b=45
+# Arithmetic operators 
+print (" the values of 3+4 is ", 3+4)
+print (" the values of 3-4 is ", 3-4)
+print (" the values of 3*4 is ", 3*4)
+print (" the values of 3/4 is ", 3/4)
+print (" the values of 3%4 is ", 3%4)
+
+# Assignment operators 
+a=44 
+a +=2
+print(a) 
+a=64 
+a -=25
+print(a)
+
+# COMPARISSON OPERATOR
+b=(14>=45)
+print(b)
+ 
+# LOGICAL OPERATOR
+bool1=True 
+bool2=False
+print("the value of bool1 and bool2 is",(bool1 and bool2))
+print("the value of bool1 and bool2 is",(bool1 or bool2))
+print("the value of bool1 and bool2 is",(not bool2))
